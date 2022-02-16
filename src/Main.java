3c67d0c74571e0776242ec4f4f3941d6cdba06de
Simon <simonenholm@hotmail.com>
2022-02-16 13:59:19 +0100

    Added menu in Main-class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hej");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("_______________________________\n\t\t- Menu -");
+        System.out.println("1. Books");
+        System.out.println("2. Shopping cart");
+        System.out.println("3. Customer Info");
+//        System.out.println("3. Exit");
+
+        System.out.print("Option: ");
+        int option = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
     }
 }
