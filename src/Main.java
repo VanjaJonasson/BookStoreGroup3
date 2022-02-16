fe1fa488e7b075da7838e16b104660dbd253bfa4
christopherhaase <christopherhaa@gmail.com>
2022-02-16 13:58:44 +0100

    Cart och ändringar i main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+
+
 public class Main {
+
+
 
     public static void main(String[] args) {
         System.out.println("hej");
+
+
+        Cart cart = new Cart();
+
+        cart.addBookToCart(new Book(1000, "Boken om grupp3", "Vi i gruppen", 199));
+
+        cart.addNewBooks(new Book(3, 35, "Bilar", "Åke", 300));
+
+        List<Book> books = cart.getBooks();
+
+
     }
 }
