abc1d5a32d5f176cc0d51995efc38c0e76ac4c25
johanna <johannautter@gmail.com>
2022-02-16 14:40:13 +0100

    BookStore class

---
+++ src/BookStore.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookStore {
+
+
+    List<Customer> customers = new ArrayList<>();
+    public static void addNewCustomer(){
+
+    }
+
+}
